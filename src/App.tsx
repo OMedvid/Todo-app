51bc194ccdb19f4584c4a24ac4bb39f6dcd648b1
Medvid Oleksandr <samaster010604@gmail.com>
2026-03-20 18:08:02 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useContext, useMemo, useState } from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Header } from './components/Header';
+import { Main as TodoList } from './components/todoList';
+import { Footer } from './components/Footer';
+import { IsActiveTab } from './types';
+import { TodosContext } from './Store';
+import { filterTodos } from './utils/filterTodos';
 
 export const App: React.FC = () => {
+  const { todos } = useContext(TodosContext);
+  const [isActive, setIsActiveTab] = useState(IsActiveTab.All);
+
+  const visibleTodos = useMemo(
+    () => filterTodos(todos, isActive),
+    [todos, isActive],
+  );
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {todos.length > 0 && <TodoList filteredTodos={visibleTodos} />}
+
+        {todos.length > 0 && (
+          <Footer isActive={isActive} setIsActiveTab={setIsActiveTab} />
+        )}
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,18 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import './styles/index.scss';
+import './styles/todo.scss';
+import './styles/filter.scss';
+import './styles/todoapp.scss';
+import { TodosProvider } from './Store';
+
+import { App } from './App';
+import React from 'react';
+
+const container = document.getElementById('root') as HTMLDivElement;
+
+createRoot(container).render(
+  <TodosProvider>
+    <App />
+  </TodosProvider>,
+);
